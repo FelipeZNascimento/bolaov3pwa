600d6f76ff909d0befb1e23b05dc587987a47ec9
Felipe <sharpion.k@gmail.com>
2021-04-13 23:43:31 +0100

    Adding menu on mobile + Login on both platforms

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,78 +1,42 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { isMobile } from "react-device-detect";
+
 import classNames from 'classnames';
 
 // Components
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import { Avatar, TextField, Tooltip } from '@material-ui/core';
+import Login from './components/Login';
 import {
-    Close as CloseIcon,
-    Check as CheckIcon,
+    Button,
+    SwipeableDrawer as Drawer,
+    TextField,
+    Tooltip
+} from '@material-ui/core';
+import {
     Person as PersonIcon,
-    ExitToApp as ExitToAppIcon,
+    Menu as MenuIcon,
     Settings as SettingsIcon,
 } from '@material-ui/icons';
 
 import ROUTES from 'constants/routes';
 import styles from './Menu.module.scss';
-// import variables from 'styles/variables.scss';
+import { withStyles } from '@material-ui/core/styles';
 import { TMenuButton } from './types';
 
-const useStyles = makeStyles(() => ({
-    default: {
-        color: '#cfd8dc',
-        backgroundColor: '#545859',
-        cursor: 'pointer',
-    },
-    red: {
-        color: '#cfd8dc',
-        backgroundColor: '#be2a2a',
-        cursor: 'pointer',
-    },
-    confirm: {
-        color: '#cfd8dc',
-        backgroundColor: '#197b30',
-        cursor: 'pointer',
-    },
-    blue: {
-        color: '#cfd8dc',
-        backgroundColor: '#1565c0',
-        cursor: 'pointer'
-    },
-    logged: {
-        color: '#cfd8dc',
-        backgroundColor: '#197b30',
-    }
-}));
-
-const CustomTextField = withStyles({
+const CustomDrawer = withStyles({
     root: {
-        '& label.Mui-focused': {
-            color: 'black',
-        },
-        '& .MuiOutlinedInput-root': {
-            '& input': {
-                color: 'white'
-            },
-            '& fieldset': {
-                borderColor: 'grey',
-            },
-            '&:hover fieldset': {
-                borderColor: 'white',
-            },
-            '&.Mui-focused fieldset': {
-                borderColor: 'white',
-            },
-        },
-    },
-})(TextField);
+        '& .MuiDrawer-paper': {
+            backgroundColor: '#1b2f42',
+        }
+    }
+})(Drawer);
 
 const Menu = () => {
-    const [loginFieldOpen, setLoginFieldOpen] = useState<boolean>(false);
     const [isLogged, setIsLogged] = useState<boolean>(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+    const [mobileLoginOpen, setMobileLoginOpen] = useState<boolean>(false);
 
     const { pathname } = useLocation();
-    const iconClasses = useStyles();
 
     const menuOptions: TMenuButton[] = [
         {
@@ -101,6 +65,69 @@ const Menu = () => {
         },
     ];
 
+    const renderMobileMenu = () => {
+        return (
+            <CustomDrawer
+                anchor='left'
+                open={mobileMenuOpen}
+                onClose={() => setMobileMenuOpen(false)}
+                onOpen={() => setMobileMenuOpen(true)}
+            >
+                <div className={styles.buttonSectionMobile}>
+                    {menuOptions.map((item) => renderButton(item))}
+                </div>
+            </CustomDrawer>
+        )
+    };
+
+    const renderMobileLogin = () => {
+        return (
+            <CustomDrawer
+                anchor='right'
+                open={mobileLoginOpen}
+                onClose={() => setMobileLoginOpen(false)}
+                onOpen={() => setMobileLoginOpen(true)}
+            >
+                <Login
+                    isLogged={isLogged}
+                    isMobile={isMobile}
+                    onClose={() => setMobileLoginOpen(false)}
+                    onLogin={setIsLogged}
+                />
+            </CustomDrawer>
+        )
+    };
+
+    const renderMobileMenuButton = () => (
+        <div className={styles.menuIcon} onClick={() => setMobileMenuOpen(true)}>
+            <Button
+                classes={{ root: 'color-grey3' }}
+                startIcon={<MenuIcon />}
+            >
+                Menu
+            </Button>
+        </div>
+    );
+
+    const renderLoginButton = () => {
+        const buttonClass = classNames(
+            [styles.menuIcon], {
+            [styles.button]: !isMobile
+        });
+
+        return (
+            <div className={buttonClass} onClick={() => setMobileLoginOpen(true)}>
+                <Button
+                    classes={{ root: isLogged ? 'color-green' : 'color-grey2' }}
+                    startIcon={<PersonIcon />}
+                    variant={isLogged ? "contained" : "outlined"}
+                >
+                    {isLogged ? 'Felipe' : 'Login'}
+                </Button>
+            </div>
+        )
+    };
+
     const renderButton = (item: TMenuButton) => {
         const buttonClass = classNames(
             [styles.button], {
@@ -109,63 +136,22 @@ const Menu = () => {
 
         return (
             <Link to={item.route}>
-                <div className={buttonClass}>{item.display}</div>
+                <div className={buttonClass} onClick={() => setMobileMenuOpen(false)}>{item.display}</div>
             </Link>
-        );
-    };
-
-    const renderLogin = () => {
-        if (isLogged) {
-            return (
-                <div className={styles.loginSection}>
-                    <Avatar className={iconClasses.blue} variant="rounded">
-                        <Tooltip title="Configurações"><SettingsIcon /></Tooltip>
-                    </Avatar>
-                    <Avatar className={iconClasses.red} variant="rounded" onClick={() => setIsLogged(false)}>
-                        <Tooltip title="Desconectar"><ExitToAppIcon /></Tooltip>
-                    </Avatar>
-                </div>
-            );
-        }
-
-        if (loginFieldOpen) {
-            return (
-                <div className={styles.loginSection}>
-                    <Avatar className={iconClasses.blue} variant="rounded" onClick={() => setLoginFieldOpen(false)}>
-                        <Tooltip title="Registrar"><PersonIcon /></Tooltip>
-                    </Avatar>
-                    <div className={styles.form}>
-                        <CustomTextField id="outlined-basic" type="email" label="email" variant="outlined" />
-                        <CustomTextField id="outlined-basic" type="password" label="password" variant="outlined" />
-                    </div>
-                    <Avatar className={iconClasses.confirm} variant="rounded" onClick={() => setIsLogged(true)}>
-                        <Tooltip title="Confirmar"><CheckIcon /></Tooltip>
-                    </Avatar>
-                    <Avatar className={iconClasses.red} variant="rounded" onClick={() => setLoginFieldOpen(false)}>
-                        <Tooltip title="Fechar"><CloseIcon /></Tooltip>
-                    </Avatar>
-                </div>
-            )
-        }
-
-        return (
-            <div className={styles.loginSection} onClick={() => setLoginFieldOpen(true)}>
-                <Avatar className={iconClasses.default} variant="rounded">
-                    <PersonIcon />
-                </Avatar>
-                <div className={styles.button}>
-                    Login
-                </div>
-            </div>
         );
     };
 
     return (
         <div className={styles.container}>
             <div className={styles.buttonSection}>
-                {menuOptions.map((item) => renderButton(item))}
+                {isMobile ? renderMobileMenuButton() : menuOptions.map((item) => renderButton(item))}
             </div>
-            {renderLogin()}
+            <div className={styles.loginSection}>
+                {renderLoginButton()}
+                {/* {isMobile ? renderMobileLoginButton() : <Login onLogin={setIsLogged} isLogged={isLogged} />} */}
+            </div>
+            {renderMobileMenu()}
+            {renderMobileLogin()}
         </div>
     );
 };
---
+++ src/components/menu/components/Login.tsx
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+
+import {
+    Close as CloseIcon,
+    Check as CheckIcon,
+    Person as PersonIcon,
+    ExitToApp as ExitToAppIcon,
+    Settings as SettingsIcon,
+} from '@material-ui/icons';
+
+import {
+    Avatar,
+    Button,
+    TextField,
+    Tooltip
+} from '@material-ui/core';
+
+import styles from './Login.module.scss';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        flex: 1,
+        margin: '4px'
+    },
+    default: {
+        color: '#cfd8dc',
+        backgroundColor: '#545859',
+        cursor: 'pointer',
+    },
+    red: {
+        color: '#cfd8dc',
+        backgroundColor: '#be2a2a',
+        cursor: 'pointer',
+    },
+    green: {
+        color: '#cfd8dc',
+        backgroundColor: '#197b30',
+        cursor: 'pointer',
+    },
+    blue: {
+        color: '#cfd8dc',
+        backgroundColor: '#1565c0',
+        cursor: 'pointer'
+    },
+    logged: {
+        color: '#cfd8dc',
+        backgroundColor: '#197b30',
+    }
+}));
+
+const CustomTextField = withStyles({
+    root: {
+        '& label': {
+            color: 'lightgrey',
+        },
+        '& label.Mui-focused': {
+            color: 'grey',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& input': {
+                color: 'white'
+            },
+            '& fieldset': {
+                borderColor: 'grey',
+            },
+            '&:hover fieldset': {
+                borderColor: 'white',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: 'white',
+            },
+        },
+    },
+})(TextField);
+
+type TProps = {
+    isLogged: boolean;
+    isMobile?: boolean;
+    onClose: (flag: boolean) => void;
+    onLogin: (flag: boolean) => void;
+}
+
+const Login = ({
+    isLogged = false,
+    isMobile = false,
+    onClose,
+    onLogin
+}: TProps) => {
+    const iconClasses = useStyles();
+
+    const renderUserOptions = () => (
+        <div className={styles.form}>
+            <Button className={`${iconClasses.root} ${iconClasses.green}`} variant="contained">
+                <PersonIcon /> Felipe
+            </Button>
+            <Button className={`${iconClasses.root} ${iconClasses.blue}`} variant="contained">
+                <SettingsIcon /> Preferências
+                </Button>
+            <Button className={`${iconClasses.root} ${iconClasses.red}`} variant="contained" onClick={() => onLogin(false)}>
+                <ExitToAppIcon /> Logout
+            </Button>
+        </div>
+    );
+
+    const renderForm = () => (
+        <div className={styles.form}>
+            <CustomTextField id="outlined-basic" type="email" label="email" variant="outlined" />
+            <CustomTextField id="outlined-basic" type="password" label="password" variant="outlined" />
+            <div className={styles.buttonContainer}>
+                {/* <Avatar className={`${iconClasses.root} ${iconClasses.blue}`} variant="rounded">
+                    <Tooltip title="Registrar"><PersonIcon /></Tooltip>
+                </Avatar> */}
+                <Button className={`${iconClasses.root} ${iconClasses.green}`} variant="contained" onClick={() => {onLogin(true); onClose(true)}}>
+                    <CheckIcon />
+                </Button>
+                <Button className={`${iconClasses.root} ${iconClasses.red}`} variant="contained" onClick={() => onClose(true)}>
+                    <CloseIcon />
+                </Button>
+            </div>
+            <Button className={`${iconClasses.root} ${iconClasses.blue}`} variant="contained">
+                <PersonIcon /> Registre-se
+            </Button>
+        </div>
+    );
+
+    if (isLogged) {
+        return renderUserOptions();
+    }
+
+    return renderForm();
+    // }
+
+    // return (
+    //     <>
+    //         <Avatar className={iconClasses.default} variant="rounded" onClick={() => setLoginFieldOpen(true)}>
+    //             <PersonIcon />
+    //         </Avatar>
+    //         <div className={styles.button} onClick={() => setLoginFieldOpen(true)}>
+    //             Login
+    //         </div>
+    //     </>
+    // );
+};
+
+export default Login;
