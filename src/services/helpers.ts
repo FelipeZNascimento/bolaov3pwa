81db0e82132e064981d635b6ee1749e466e977f6
Felipe <sharpion.k@gmail.com>
2021-07-31 15:21:20 +0100

    Adding GUID generator

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -3,4 +3,12 @@ const validateEmail = (email: string) => {
     return re.test(String(email).toLowerCase());
 };
 
-export default validateEmail;
+const guidGenerator = () => {
+    const S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+};
+
+
+export { guidGenerator, validateEmail };
