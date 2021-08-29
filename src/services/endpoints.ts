b79d2400154654b40e6d311b625ba4f7a1acc5a2
Felipe <sharpion.k@gmail.com>
2021-08-29 14:05:58 +0100

    Updating endpoint for new API

--- src/services/endpoints.ts
+++ src/services/endpoints.ts
@@ -1,5 +1,5 @@
-// export const apiBaseUrl = 'http://localhost:8081/bolaonfl/';
-export const apiBaseUrl = 'https://omega-cors-nfl.herokuapp.com/bolaonfl/';
+// export const apiBaseUrl = 'http://localhost:56992/bolaonfl/';
+export const apiBaseUrl = 'https://apibolao.omegafox.me/bolaonfl/';
 
 export const config = () => `${apiBaseUrl}defaultConfig/`;
 
