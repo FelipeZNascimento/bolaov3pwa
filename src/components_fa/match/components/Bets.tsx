e215e0c203ac179caee9bd5156607408f8b039f5
Felipe (Home) <sharpion.k@gmail.com>
2023-09-12 14:15:20 +0100

    Fix mobile display bug + remove preseason

--- src/components_fa/match/components/Bets.tsx
+++ src/components_fa/match/components/Bets.tsx
@@ -1,72 +1,84 @@
 import React from 'react';
-import { isMobile } from "react-device-detect";
+import { isMobile } from 'react-device-detect';
 import classNames from 'classnames';
 
-import {
-    Icon
-} from '@material-ui/core';
+import { Icon } from '@material-ui/core';
 
 import { TMatchBet } from 'store/matches/types';
 import { BETS_VALUES } from 'constants/bets';
 import styles from './Bets.module.scss';
 
 type TProps = {
-    bets: TMatchBet[],
-    correctBets: {
-        bullseye: number[],
-        half: number[]
-    },
-    loggedUserBets?: null | TMatchBet,
-}
-const Bets = ({
-    bets,
-    correctBets,
-    loggedUserBets = null
-}: TProps) => {
-    const renderBetLine = (bet: TMatchBet) => (
-        <div className={styles.betLine} key={`${bet.matchId}${bet.user.id}${bet.id}`}>
-            {!isMobile && <Icon classes={{ root: styles.iconClass }} fontSize="small" className={bet.user.icon} style={{ color: bet.user.color }} />}
-            <span className={styles.userName}>{bet.user.name}</span>
-        </div>
-    );
+  bets: TMatchBet[];
+  correctBets: {
+    bullseye: number[];
+    half: number[];
+  };
+  loggedUserBets?: null | TMatchBet;
+};
+const Bets = ({ bets, correctBets, loggedUserBets = null }: TProps) => {
+  const renderBetLine = (bet: TMatchBet) => (
+    <div
+      className={styles.betLine}
+      key={`${bet.matchId}${bet.user.id}${bet.id}`}
+    >
+      {!isMobile && (
+        <Icon
+          classes={{ root: styles.iconClass }}
+          fontSize="small"
+          className={bet.user.icon}
+          style={{ color: bet.user.color }}
+        />
+      )}
+      <span className={styles.userName}>{bet.user.name}</span>
+    </div>
+  );
 
-    const renderColumns = (columnBet: number, columnTitle: string) => {
-        const isBullseyeColumn = correctBets.bullseye.find((correctBet) => correctBet === columnBet) !== undefined;
-        const isHalfColumn = correctBets.half.find((correctBet) => correctBet === columnBet) !== undefined;
+  const renderColumns = (columnBet: number, columnTitle: string) => {
+    const isBullseyeColumn =
+      correctBets.bullseye.find((correctBet) => correctBet === columnBet) !==
+      undefined;
+    const isHalfColumn =
+      correctBets.half.find((correctBet) => correctBet === columnBet) !==
+      undefined;
 
-        const columnClass = classNames({
-            [styles.columnBullseye]: isBullseyeColumn,
-            [styles.column]: !isBullseyeColumn,
-        });
+    const columnClass = classNames({
+      [styles.columnBullseye]: isBullseyeColumn,
+      [styles.column]: !isBullseyeColumn
+    });
 
-        const columnHeaderClass = classNames({
-            [styles.headerBullseye]: isBullseyeColumn,
-            [styles.headerHalf]: isHalfColumn,
-            [styles.headerZero]: !isHalfColumn && !isBullseyeColumn,
-        });
-
-        return (
-            <div className={columnClass}>
-                <div className={columnHeaderClass}>
-                    {columnTitle}
-                </div>
-                {loggedUserBets && loggedUserBets.value === columnBet && renderBetLine(loggedUserBets)}
-                {bets
-                    .filter((bet) => bet.value === columnBet)
-                    .map((bet) => renderBetLine(bet))
-                }
-            </div>
-        )
-    }
+    const columnHeaderClass = classNames({
+      [styles.headerBullseye]: isBullseyeColumn,
+      [styles.headerHalf]: isHalfColumn,
+      [styles.headerZero]: !isHalfColumn && !isBullseyeColumn
+    });
 
     return (
-        <div className={styles.container}>
-            {renderColumns(BETS_VALUES.AWAY_EASY, 'Fácil')}
-            {renderColumns(BETS_VALUES.AWAY_HARD, 'Difícil')}
-            {renderColumns(BETS_VALUES.HOME_HARD, 'Difícil')}
-            {renderColumns(BETS_VALUES.HOME_EASY, 'Fácil')}
-        </div >
-    )
-}
+      <div className={columnClass}>
+        <div className={columnHeaderClass}>{columnTitle}</div>
+        {loggedUserBets &&
+          loggedUserBets.value === columnBet &&
+          renderBetLine(loggedUserBets)}
+        {bets
+          .filter((bet) => bet.value === columnBet)
+          .map((bet) => renderBetLine(bet))}
+      </div>
+    );
+  };
+
+  const containerClass = classNames(styles.container, {
+    [styles.containerMobile]: isMobile,
+    [styles.containerStandard]: !isMobile
+  });
+
+  return (
+    <div className={containerClass}>
+      {renderColumns(BETS_VALUES.AWAY_EASY, 'Fácil')}
+      {renderColumns(BETS_VALUES.AWAY_HARD, 'Difícil')}
+      {renderColumns(BETS_VALUES.HOME_HARD, 'Difícil')}
+      {renderColumns(BETS_VALUES.HOME_EASY, 'Fácil')}
+    </div>
+  );
+};
 
 export default Bets;
