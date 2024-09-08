2d3b749eeabbff53dc82f29452ae62d5cd080908
Felipe (Home) <sharpion.k@gmail.com>
2024-09-08 19:10:19 +0100

    feat(): Adding adsense

---
+++ src/components_fa/ranking/RankingAd.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class AdComponent extends React.Component {
+  componentDidMount() {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
+  render() {
+    return (
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-ad-client="ca-pub-9713954775329576"
+        data-ad-slot="2776132415"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+        data-adtest="on"
+      />
+    );
+  }
+}
