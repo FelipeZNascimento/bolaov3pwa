01b7c09aacfb5c71398f87d750beb8977fd125ad
Nelson Motta <ngm.motta@gmail.com>
2025-07-30 17:19:49 -0300

    Enabling 2025 preseason, hiding playoff weeks and updating season

--- src/sections/rules/components/Subscription.tsx
+++ src/sections/rules/components/Subscription.tsx
@@ -5,13 +5,13 @@ const Subscription = () => {
     <div className={styles.internalRules}>
       <h3 className={styles.highlight}>Inscrição</h3>
       <div className={styles.text}>
-        Estão abertas as inscrições para a temporada 2024/2025 do Bolão NFL!
+        Estão abertas as inscrições para a temporada 2025/2026 do Bolão NFL!
         <br />
         Após fazer o cadastro, sua participação estará condicionada ao pagamento
         da inscrição.
         <br />
         <br />
-        Valor: <span className={styles.highlight}>R$60</span>
+        Valor: <span className={styles.highlight}>R$75</span>
         <br />
         <br />
         Nelson Gimenez da Motta - PIX 395.825.028-94
